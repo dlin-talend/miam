3ddba4e849056a8f8ed14e03bd1a32b4b1525770
ldong-talend <ldong@talend.com>
2014-06-10 18:24:32 +0800

    TUP-5 commited:change the country.

--- src/my/country/Country.java
+++ src/my/country/Country.java
@@ -1,5 +1,16 @@
 package my.country;
 /*Exercise M3*/
 public enum Country {
-	France, Italy, China, Spain;
+	
+	France("france"), Italy("italy"), China("china"), Spain("spain");
+	
+	
+	private String label;
+	Country(String label){
+		this.label = label;
+	}
+	
+	public String getLabel(){
+		return this.label;
+	}
 }
